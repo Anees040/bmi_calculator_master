457df8a7d02bc148bc08434b39ea44cee13ca6ab
sp23-bse-030 <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-03-28 00:00:56 +0500

    chore: update lib/input_page/input_page_styles.dart

--- lib/input_page/input_page_styles.dart
+++
@@ -1,6 +0,0 @@
-import 'package:bmi_calculator/widget_utils.dart';
-import 'package:flutter/material.dart';
-
-double appBarHeight(BuildContext context) {
-  return screenAwareSize(80.0, context) + MediaQuery.of(context).padding.top;
-}
